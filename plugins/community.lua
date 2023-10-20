c11b76b35c8982206331bcea8f72330691372dec
Alexis Guzman <codingcodax@gmail.com>
2023-10-19 18:14:27 -0600

    feat(plugins): import plugins from astro community

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,23 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	"AstroNvim/astrocommunity",
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+	{ import = "astrocommunity.color.twilight-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.completion.codeium-vim" },
+	{ import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+	{ import = "astrocommunity.editing-support.zen-mode-nvim" },
+	{ import = "astrocommunity.git.openingh-nvim" },
+	{ import = "astrocommunity.media.vim-wakatime" },
+	{ import = "astrocommunity.pack.astro" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.prisma" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.scrolling.nvim-scrollbar" },
 }
